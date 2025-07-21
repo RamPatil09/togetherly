bcbc1b58a9a86c558730f61357704bfee996038f
RamPatil09 <ramangoudpatil.xworkz@gmail.com>
2025-07-21 20:08:37 +0530

    Adding togetherly application

---
+++ src/main/java/com/socialmedia/togetherly/TogetherlyApplication.java
@@ -0,0 +1,13 @@
+package com.socialmedia.togetherly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogetherlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/socialmedia/togetherly/TogetherlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialmedia.togetherly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogetherlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
